1a976724a39c25c1e4d8d3047d08b02b70e039f3
Giuliano Losa <giuliano@losa.fr>
2020-10-26 20:50:50 -0700

    add core-verify to memory_safety.saw

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -6,9 +6,9 @@ rm -r build/llvm
 cp -r blst build/llvm
 cd build/llvm
 
-for p in ../../patches/*
+for p in /workdir/patches/*
 do
-  patch -f -p1 -t < /workdir/patches/"$p" # -f to prevent patch from automatically enabling option -R when it things it should
+  patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
 done
 
 export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
