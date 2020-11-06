d01251068ff5dfa7a2cba11b0c06f485586b5b6f
Giuliano Losa <giuliano@losa.fr>
2020-11-06 11:38:52 -0800

    add minimal reporting

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -5,5 +5,7 @@ export PATH=/workdir/bin:$PATH
 
 ./scripts/build_llvm.sh
 ./scripts/build_x86.sh
+echo "Proving C and assembly functions"
 ./scripts/prove.sh
+echo "Validating Cryptol specifications"
 ./scripts/check.sh | if grep False; then exit 1; fi # look for any failed checks
--- scripts/prove.sh
+++ scripts/prove.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -e
 
+err_handler () {
+    [ $? -eq 0 ] && exit
+    echo "Verification failed"
+}
+
+trap err_handler EXIT
+
 saw proof/memory_safety.saw
 saw proof/keygen.saw
+
+echo "All functions under verification were verified successfully"
