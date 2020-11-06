ef396c5f1674b221c941c01f7dc1e0bd5a168679
Giuliano Losa <giuliano@losa.fr>
2020-11-06 12:24:10 -0800

    improve summary output

--- scripts/prove.sh
+++ scripts/prove.sh
@@ -3,12 +3,20 @@ set -e
 
 err_handler () {
     [ $? -eq 0 ] && exit
-    echo "Verification failed"
+    echo "Verification failed in $file"
 }
 
 trap err_handler EXIT
 
-saw proof/memory_safety.saw
-saw proof/keygen.saw
+files="proof/memory_safety.saw proof/keygen.saw"
+
+for f in $files; do
+  file=$f
+  saw $f
+done
 
 echo "All functions under verification were verified successfully"
+echo "Files processed:"
+for f in $files; do
+  echo $f
+done
