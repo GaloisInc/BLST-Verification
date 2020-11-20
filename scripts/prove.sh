23d701e037107c30a262d40c67772bd73545e2fc
Giuliano <giuliano@losa.fr>
2020-11-20 10:43:04 -0800

    Reorganize mem safety (#25)

--- scripts/prove.sh
+++ scripts/prove.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 set -e
 
-saw proof/memory_safety.saw
-saw proof/keygen.saw
-saw proof/hash_to_field.saw
+err_handler () {
+    [ $? -eq 0 ] && exit
+    echo "Verification failed in $file"
+}
+
+trap err_handler EXIT
+
+files="proof/memory_safety.saw proof/keygen.saw"
+
+for f in $files; do
+  file=$f
+  saw $f
+done
+
+echo "All functions under verification were verified successfully"
+echo "Files processed:"
+for f in $files; do
+  echo $f
+done
