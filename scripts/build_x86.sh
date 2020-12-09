20c5636c9ce3eb5b5339f57279a363736c835e13
Samuel Breese <samuel@chame.co>
2020-12-09 15:19:44 -0500

    Manually pass -D__ADX__ in CFLAGS instead of reading /proc/cpuinfo

--- scripts/build_x86.sh
+++ scripts/build_x86.sh
@@ -6,7 +6,8 @@ rm -r build/x86
 cp -r blst build/x86
 cd build/x86
 
-export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
+export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
 export CC=clang
 sed -i'' 's/^trap/# trap/' build.sh
+sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
 ./build.sh -shared
