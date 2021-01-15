bc2b5c92eaff271a3f473ff208efe1eda5c6bb71
Samuel Breese <samuel@chame.co>
2021-01-15 14:18:39 -0500

    Add memory safety proofs for mulq implementations

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -1,18 +1,40 @@
 #!/bin/sh
 set -e
 
-mkdir -p build/llvm
-rm -r build/llvm
-cp -r blst build/llvm
-cd build/llvm
+(
+    mkdir -p build/llvm
+    rm -r build/llvm
+    cp -r blst build/llvm
+    cd build/llvm
 
-for p in /workdir/patches/*
-do
-  patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
-done
+    for p in /workdir/patches/*
+    do
+        patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
+    done
 
-export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
-export CC=wllvm
-export LLVM_COMPILER=clang
-./build.sh
-extract-bc --bitcode libblst.a
+    export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
+    export CC=wllvm
+    export LLVM_COMPILER=clang
+    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+    ./build.sh
+    extract-bc --bitcode libblst.a
+)
+
+(
+    mkdir -p build/llvm_noadx
+    rm -r build/llvm_noadx
+    cp -r blst build/llvm_noadx
+    cd build/llvm_noadx
+
+    for p in /workdir/patches/*
+    do
+        patch -f -p1 -t < "$p"
+    done
+
+    export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
+    export CC=wllvm
+    export LLVM_COMPILER=clang
+    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+    ./build.sh
+    extract-bc --bitcode libblst.a
+)
--- scripts/build_x86.sh
+++ scripts/build_x86.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 set -e
 
-mkdir -p build/x86
-rm -r build/x86
-cp -r blst build/x86
-cd build/x86
+(
+    mkdir -p build/x86
+    rm -r build/x86
+    cp -r blst build/x86
+    cd build/x86
 
-export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
-export CC=clang
-sed -i'' 's/^trap/# trap/' build.sh
-sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
-./build.sh -shared
+    export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
+    export CC=clang
+    sed -i'' 's/^trap/# trap/' build.sh
+    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+    ./build.sh -shared
+)
+
+(
+    mkdir -p build/x86_noadx
+    rm -r build/x86_noadx
+    cp -r blst build/x86_noadx
+    cd build/x86_noadx
+
+    export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
+    export CC=clang
+    sed -i'' 's/^trap/# trap/' build.sh
+    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+    ./build.sh -shared
+)
