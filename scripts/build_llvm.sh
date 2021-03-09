ce8ccc683ac64db35a4859657d23a3a345b59e8e
Samuel Breese <samuel@chame.co>
2021-03-09 17:43:58 -0500

    Add memory safety proof for ctx_inverse_mod_384

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -1,40 +1,30 @@
 #!/bin/sh
 set -e
 
-(
-    mkdir -p build/llvm
-    rm -r build/llvm
-    cp -r blst build/llvm
-    cd build/llvm
+build () {
+    (
+        mkdir -p "build/$1"
+        rm -r "build/$1"
+        cp -r "$2" "build/$1"
+        cd "build/$1"
 
-    for p in /workdir/patches/*
-    do
-        patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
-    done
+        if [ "$2" = blst ]; then
+            for p in ../../patches/*
+            do
+                patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
+            done
+        fi
 
-    export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
-    export CC=wllvm
-    export LLVM_COMPILER=clang
-    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
-    ./build.sh
-    extract-bc --bitcode libblst.a
-)
+        export CFLAGS="-g -fPIC -Wall -Wextra -Werror $3"
+        export CC=wllvm
+        export LLVM_COMPILER=clang
+        sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+        ./build.sh
+        extract-bc --bitcode libblst.a
+    )
+}
 
-(
-    mkdir -p build/llvm_noadx
-    rm -r build/llvm_noadx
-    cp -r blst build/llvm_noadx
-    cd build/llvm_noadx
-
-    for p in /workdir/patches/*
-    do
-        patch -f -p1 -t < "$p"
-    done
-
-    export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
-    export CC=wllvm
-    export LLVM_COMPILER=clang
-    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
-    ./build.sh
-    extract-bc --bitcode libblst.a
-)
+build llvm blst '-D__ADX__'
+build llvm_noadx blst
+build llvm_patched blst_patched '-D__ADX__'
+build llvm_noadx_patched blst_patched
--- scripts/build_x86.sh
+++ scripts/build_x86.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 set -e
 
-(
-    mkdir -p build/x86
-    rm -r build/x86
-    cp -r blst build/x86
-    cd build/x86
+build () {
+    (
+        mkdir -p "build/$1"
+        rm -r "build/$1"
+        cp -r "$2" "build/$1"
+        cd "build/$1"
 
-    patch -f -p1 -t < ../../patches/noxmmptr.patch
+        if [ "$2" = blst ]; then
+            patch -f -p1 -t < ../../patches/noxmmptr.patch
+        fi
 
-    export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
-    export CC=clang
-    sed -i'' 's/^trap/# trap/' build.sh
-    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
-    ./build.sh -shared
-)
+        export CFLAGS="-g -fPIC -Wall -Wextra -Werror $3"
+        export CC=clang
+        sed -i'' 's/^trap/# trap/' build.sh
+        sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+        ./build.sh -shared
+    )
+}
 
-(
-    mkdir -p build/x86_noadx
-    rm -r build/x86_noadx
-    cp -r blst build/x86_noadx
-    cd build/x86_noadx
-
-    patch -f -p1 -t < ../../patches/noxmmptr.patch
-
-    export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
-    export CC=clang
-    sed -i'' 's/^trap/# trap/' build.sh
-    sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
-    ./build.sh -shared
-)
+build x86 blst '-D__ADX__'
+build x86_noadx blst
+build x86_patched blst_patched '-D__ADX__'
+build x86_noadx_patched blst_patched
