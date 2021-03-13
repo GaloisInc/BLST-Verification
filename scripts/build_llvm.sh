f17a35685bebd91e3abab2cf0ca262cbdb85b47c
Sam Breese <chameco@users.noreply.github.com>
2021-03-12 19:30:29 -0500

    Add memory safety proofs for remaining x86_64 routines (#39)
    * Add memory safety proofs for mulq implementations
    
    * Include patch that avoids offloading return value pointer in XMM
    
    * Add memory safety proof for ctx_inverse_mod_384
    
    * Use new x86 What4 hash consing option
    
    * Update README.md, document options in ctx_inverse_mod_384.saw
    
    * Update SAW_URL
    
    * Update Docker configuration
    
    * Fix typo
    
    * blst_patched -> blst_recent
    
    * Expand upon comment in ctx_inverse_mod_384.saw
    
    * Remove some now-redundant TODO comments
    
    * Add comment to README.md

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 set -e
 
-mkdir -p build/llvm
-rm -r build/llvm
-cp -r blst build/llvm
-cd build/llvm
+build () {
+    (
+        mkdir -p "build/$1"
+        rm -r "build/$1"
+        cp -r "$2" "build/$1"
+        cd "build/$1"
 
-for p in /workdir/patches/*
-do
-  patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
-done
+        if [ "$2" = blst ]; then
+            for p in ../../patches/*
+            do
+                patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it things it should
+            done
+        fi
 
-export CFLAGS='-g -fPIC -Wall -Wextra -Werror -D__ADX__'
-export CC=wllvm
-export LLVM_COMPILER=clang
-./build.sh
-extract-bc --bitcode libblst.a
+        export CFLAGS="-g -fPIC -Wall -Wextra -Werror $3"
+        export CC=wllvm
+        export LLVM_COMPILER=clang
+        sed -i'' 's/^CFLAGS=/# CFLAGS=/' build.sh
+        ./build.sh
+        extract-bc --bitcode libblst.a
+    )
+}
+
+build llvm blst '-D__ADX__'
+build llvm_noadx blst
+build llvm_recent blst_recent '-D__ADX__'
+build llvm_noadx_recent blst_recent
--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ if [ $# -ne 0 ] && [ "$1" = "--latest" ]; then
   CRYPTOL_NIGHTLY=$(curl -s https://cryptol.net/builds/nightly/ | grep -oP "cryptol.*?${CRYPTOL_DATE}-Ubuntu.*?\.tar\.gz"  | head -n 1) # `curl -s` means silent; `grep -o` says print only the matched substring; `grep -P` says Perl syntax, which we use to get a lazy match (shortest) with .*?
   CRYPTOL_URL="https://cryptol.net/builds/nightly/${CRYPTOL_NIGHTLY}"
 else
-  SAW_URL="https://saw.galois.com/builds/nightly/saw-0.7.0.99-2021-01-06-Linux-x86_64.tar.gz"
+  SAW_URL="https://saw.galois.com/builds/nightly/saw-0.7.0.99-2021-03-11-Linux-x86_64.tar.gz"
   CRYPTOL_URL="https://github.com/GaloisInc/cryptol/releases/download/2.9.1/cryptol-2.9.1-Linux-x86_64.tar.gz"
 
 fi
