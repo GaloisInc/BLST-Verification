3b2e4ba6cdc63fffae5576dc550a77604800810b
Brett Boston <boston@galois.com>
2021-05-24 17:19:50 -0700

    Squash down for rebase
    Squash down for cherrypick
    
    bls_operations building + initial proof work
    
    Start tracking c file
    
    CoreSign_A spec
    
    Fix blst_sk_to_pk_in_g2 abort
    
    KeyValidate_A proof
    
    Postcond over portion of the code
    
    Argh
    
    is_point_O stuff goes through now
    
    Minor cleanup
    
    Getting closer...
    
    Get KeyValidate proof to go through with some assumptions
    
    Get hash_to_g1 override to apply
    
    All overrides apply
    
    Some progress on SkToPk_a
    
    Goes through with simplified precond
    
    With one more precond
    
    Final precond
    
    working sk to pk proof
    
    Some cleanup
    
    sign_pk_in_g2 preconds satisfied
    
    Overrides apply
    
    Nonworking postcond attempt
    
    Fix NULL issue
    
    Account for \0 terminator at end of DST string
    
    dedup DST_A
    
    Postcond goes through
    
    Some trivial theorem proofs
    
    Fix serialize_e1_eq_thm
    
    Fix serialize_e2_eq_thm
    
    proof split_join theorem
    
    Revert deserialize changes
    
    Revert same_point_affine postcond changes
    
    Prove hash_to_curve_e1_impl_thm
    
    Add abstract precond and fix or thms
    
    inv proof
    
    Add is_point_projective precond to proof
    
    Incorperate proj_precond
    
    Cleanup and quickchecking
    
    Fix pubkey_to_point_to_pubkey_thm
    
    Prove test_thm_correct
    
    More explicitly proven test_thm
    
    Proof fixed for simplified uncompress postcond
    
    Restore postcond
    
    So close.  Proof should go through after updating or_2_thm
    
    Working SkToPk_A
    
    Prove new test_thm_correct
    
    Update deserialize_E2_uncompress_E2_OK_thm
    
    Move declaration up
    
    Revert some changes in prep for rebase
    
    Prep for rebase
    
    entrypoint updates
    
    Fix coresign_A proof
    
    Normalization ideas
    
    Add some normalization assumptions to make proofs go throuh
    
    Prove normalize_uncompress_E2_OK_thm
    
    Prove eliminate_normalized_args_affinify_E_thm
    
    Remove pubkey_to_point_to_pubkey_thm
    
    Support both compress overrides in CoreSign_A proof
    
    Prove serialize_e1_affine_thm
    
    Prove new test_thm_correct'
    
    Remove some or_* thms
    
    Revert hash_to_g1 changes

---
+++ proof/bls_operations.c
@@ -0,0 +1,134 @@
+#include "blst.h"
+#include <stdio.h> // for NULL 
+
+// TODO: Move
+
+// Two variants:
+// _A: min signature size
+// _B: min pk size
+
+// Explicitly note that demo_DST_A is 43 bytes to avoid the null terminator
+// (needed for SAW to treat the allocation as 32 bytes)
+byte demo_DST_A[43] = "BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_NUL_";
+// TODO: demo_DST_B
+
+// KeyGen maps directly to blst_keygen
+
+// SkToPk
+void demo_SkToPk_A(unsigned char out[96], const blst_scalar *SK) {
+  blst_p2 pk;
+  blst_sk_to_pk_in_g2(&pk, SK);
+  blst_p2_compress(out, &pk);
+  }
+
+void demo_SkToPk_B(unsigned char out[48], const blst_scalar *SK) {
+  blst_p1 pk;
+  blst_sk_to_pk_in_g1(&pk, SK);
+  blst_p1_compress(out, &pk);
+};
+
+// KeyValidate
+
+limb_t demo_KeyValidate_A(const unsigned char in[96]) {
+  blst_p2_affine pk;
+  BLST_ERROR r;
+  r = blst_p2_uncompress(&pk, in);
+  return r == BLST_SUCCESS &&
+         blst_p2_affine_on_curve(&pk) &&
+         !blst_p2_affine_is_inf(&pk) &&
+         blst_p2_affine_in_g2(&pk);
+  /*
+  if (r != BLST_SUCCESS) return 0;
+  if (! blst_p2_affine_on_curve(&pk)) return 0;
+  if (blst_p2_affine_is_inf(&pk)) return 0;
+  if (! blst_p2_affine_in_g2(&pk)) return 0;
+  return 1;
+  */
+};
+
+BLST_ERROR demo_KeyValidate_B(const unsigned char in[48]) {
+  blst_p1_affine pk;
+  BLST_ERROR r;
+  r = blst_p1_uncompress(&pk, in);
+  if (r != BLST_SUCCESS) return r;
+  if (! blst_p1_affine_on_curve(&pk)) return BLST_POINT_NOT_ON_CURVE;
+  if (blst_p1_affine_is_inf(&pk)) return BLST_PK_IS_INFINITY;
+  if (! blst_p1_affine_in_g1(&pk)) return BLST_POINT_NOT_IN_GROUP;
+  return BLST_SUCCESS;
+};
+
+
+// CoreSign
+
+void demo_CoreSign_A(byte out[48], const blst_scalar *SK,
+                     const byte *message, size_t message_len) {
+  // TODO: Figure out how to use globals in SAW?
+  //const byte local_DST_A[] = "BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_AUG_";
+  blst_p1 Q1, Q2;
+  // TODO: Double check, but I think aug is supposed to be a public key?  That
+  // will determine the size it should be.
+  // TODO: Is DST_len supposted to include the null terminator?  It's 44 with
+  // the null terminator, and 43 without.
+  blst_hash_to_g1(&Q1, message, message_len, demo_DST_A, 43, NULL, 0); // no "aug" string
+  // blst_p1_mult(&Q, &Q, SK, 256); // or 255?
+  blst_sign_pk_in_g2(&Q2, &Q1, SK);
+  blst_p1_compress(out, &Q2);
+};
+
+void demo_CoreSign_B(byte out[96], const blst_scalar *SK,
+                     const byte *message, size_t message_len) {
+  blst_p2 Q;
+  blst_hash_to_g2(&Q, message, message_len, demo_DST_A, 43, NULL, 0); // no "aug" string
+  // TODO: fix DST string above
+  blst_sign_pk_in_g1(&Q, &Q, SK);
+  blst_p2_compress(out, &Q);
+};
+
+// CoreVerify
+
+bool demo_CoreVerify_A(const byte sig[48], const byte pk[96], const byte *message, size_t message_len) {
+  blst_p1_affine R;
+  blst_p1 Q;
+  blst_p2_affine PK;
+  // uncompress and check the sig
+  if (blst_p1_uncompress(&R, sig) != BLST_SUCCESS) return 0;
+  if (! blst_p1_affine_on_curve(&R)) return 0;
+  if (blst_p1_affine_is_inf(&R)) return 0;
+  if (! blst_p1_affine_in_g1(&R)) return 0;
+
+  // uncompress and check the pub key
+  if (blst_p2_uncompress(&PK, pk) != BLST_SUCCESS) return 0;
+  if (! blst_p2_affine_on_curve(&PK)) return 0;
+  if (blst_p2_affine_is_inf(&PK)) return 0;
+  if (! blst_p2_affine_in_g2(&PK)) return 0;
+
+  if (blst_core_verify_pk_in_g2(&PK, &R, 1, message, message_len, demo_DST_A, 43, NULL, 0) != BLST_SUCCESS)
+    return 0;
+  return 1;
+};
+
+bool demo_CoreVerify_B(const byte sig[96], const byte pk[48], const byte *message, size_t message_len) {
+  blst_p2_affine R;
+  blst_p2 Q;
+  blst_p1_affine PK;
+  // uncompress and check the sig
+  if (blst_p2_uncompress(&R, sig) != BLST_SUCCESS) return 0;
+  if (! blst_p2_affine_on_curve(&R)) return 0;
+  if (blst_p2_affine_is_inf(&R)) return 0;
+  if (! blst_p2_affine_in_g2(&R)) return 0;
+
+  // TODO: Maybe remove the above checks if the override handles error cases.
+  // Can't remove pubkey checks?  (Same goes for A variant)
+
+  // uncompress and check the pub key
+  if (blst_p1_uncompress(&PK, pk) != BLST_SUCCESS) return 0;
+  if (! blst_p1_affine_on_curve(&PK)) return 0;
+  if (blst_p1_affine_is_inf(&PK)) return 0;
+  if (! blst_p1_affine_in_g1(&PK)) return 0;
+
+
+  // TODO: fix DST string
+  if (blst_core_verify_pk_in_g1(&PK, &R, 1, message, message_len, demo_DST_A, 43, NULL, 0) != BLST_SUCCESS)
+    return 0;
+  return 1;
+};
--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -3,9 +3,9 @@ set -e # exit immediately if a command fails
 
 export PATH=/workdir/bin:$PATH
 
-./scripts/build_llvm.sh
-./scripts/build_x86.sh
+#./scripts/build_llvm.sh
+#./scripts/build_x86.sh
 echo "Proving C and assembly functions"
 ./scripts/prove.sh
-echo "Validating Cryptol specifications"
-./scripts/check.sh | if grep False; then exit 1; fi # look for any failed checks
+#echo "Validating Cryptol specifications"
+#./scripts/check.sh | if grep False; then exit 1; fi # look for any failed checks
