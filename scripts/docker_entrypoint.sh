07fac135ff13fa0b43453311e6c3c07883855493
Brett Boston <boston@galois.com>
2021-06-22 14:57:03 -0700

    All preconditions satisfied

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -3,8 +3,8 @@ set -e # exit immediately if a command fails
 
 export PATH=/workdir/bin:$PATH
 
-#./scripts/build_llvm.sh
-#./scripts/build_x86.sh
+./scripts/build_llvm.sh
+./scripts/build_x86.sh
 echo "Proving C and assembly functions"
 ./scripts/prove.sh
 #echo "Validating Cryptol specifications"
