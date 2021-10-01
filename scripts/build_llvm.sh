5bef4a31e504db26d041df1cf0fb75b7a6b9ab46
Giuliano Losa <giuliano@galois.com>
2021-10-01 16:46:16 -0700

    add mem safety proofs for more sizes (still E1)
    also add patch to set stride to 32

--- scripts/build_llvm.sh
+++ scripts/build_llvm.sh
@@ -9,7 +9,14 @@ build () {
         cd "build/$1"
 
         if [ "$2" = blst ]; then
-            for p in ../../patches/*
+            for p in ../../patches/blst/*
+            do
+                patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it thinks it should
+            done
+        fi
+
+        if [ "$2" = blst_bulk_addition ]; then
+            for p in ../../patches/blst_bulk_addition/*
             do
                 patch -f -p1 -t < "$p" # -f to prevent patch from automatically enabling option -R when it thinks it should
             done
